e71a63514c08a8363dcd28cc99bee0337ac77dba
Ali Saif <145758021+AliSaif1@users.noreply.github.com>
2025-08-20 19:46:35 +0500

    Update SendInvitation.jsx

--- src/Layout/Brand/DashBoard/Pages/Compaign/SendInvitation.jsx
+++ src/Layout/Brand/DashBoard/Pages/Compaign/SendInvitation.jsx
@@ -2,7 +2,7 @@ import React, { useContext, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Select from 'react-select';
 import { CampaignContext } from './CurrentCompaign';
-import './index.css';
+// import './index.css';
 
 const SendInvitation = ({ influencersOptions, selectedInfluencers, spendings, campaignBudget, handleChange }) => {
   const campaignData = useContext(CampaignContext);
